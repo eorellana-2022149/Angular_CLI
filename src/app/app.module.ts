4004e6e5f4f1a8b6adfff28f57a0753c83f3c568
eorellana-2022149 <eorellana-2022149@kinal.edu.gt>
2026-08-14 23:01:03 -0600

    4 componentes mas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,22 @@ import { App } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BodyComponent } from './components/body/body.component';
+import { ButtonComponent } from './components/button/button.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListComponent } from './components/list/list.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
-  declarations: [App, HeaderComponent, FooterComponent, BodyComponent],
+  declarations: [
+    App,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    ButtonComponent,
+    NavbarComponent,
+    ListComponent,
+    FormComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
